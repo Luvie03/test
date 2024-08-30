7d5602eefee95d411df4b4c5c2d4d1a4ecd9bb0d
Prince kumar <139984438+Luvie03@users.noreply.github.com>
2024-08-30 19:16:10 +0530

    done

--- test_1/src/components/button.tsx
+++ test_1/src/components/button.tsx
@@ -2,7 +2,7 @@ const Button = () => {
   return (
     <>
       <input
-        className="w-[370px] h-[40px] bg-[#FFCE22] rounded-[4px] self-center font-montserrat"
+        className="w-[370px] h-[40px] bg-[#FFCE22] hover:opacity-75 rounded-[4px] self-center font-montserrat"
         type="button"
         value="Done"
       />
